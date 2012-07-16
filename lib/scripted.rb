fb3d805b5ed8ba0f5a58b8a27502c4d52bd9cff4
iain <iain@iain.nl>
2012-07-17 01:20:28 +0200

    Find formatters locally or remotely

--- lib/scripted.rb
+++ lib/scripted.rb
@@ -16,9 +16,6 @@ require "scripted/running/execute"
 require "scripted/logger"
 
 require "scripted/formatters/blank"
-require "scripted/formatters/default"
-require "scripted/formatters/table"
-require "scripted/formatters/announcer"
 
 module Scripted
 
